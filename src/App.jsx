2d43c1d4d8e2fa74320fc6544f18bc8284a974e5
Ruy Luna <ruyjluna@gmail.com>
2025-07-06 20:18:27 -0700

    grid styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="containers">
+        <div className="container mainContainer">
+          <h1>Welcome</h1>
+        </div>
+
+        <div className="container projects">
+          <h1>Projects </h1>
+        </div>
+
+        <div className="container links">
+          <h1>Contact me</h1>
+        </div>
+
+        <div className="container fun">
+          <h1>Fun</h1>
+        </div>
+
+        <div className="container skills">
+          <h1>Skills</h1>
+        </div>
+
+        <div className="container footer">
+          <h1>Copyright</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
